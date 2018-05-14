28856968e8059b5bcdaac08f6d567e5f137fa591
Nick Rutowski <nickr@doordash.com>
2018-05-13 19:23:41 -0700

    add scraper and jQuery

--- functions/scrape.js
+++ functions/scrape.js
@@ -1,6 +1,21 @@
+var scraper = require("scraper");
+var jQuery = require("jquery");
+
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+  try {
+    scraper("http://search.twitter.com/search?q=javascript", function(
+      error,
+      jQuery
+    ) {
+      if (error) {
+        callback(error);
+      }
+      callback(null, {
+        statusCode: 200,
+        body: jQuery(".msg").length
+      });
+    });
+  } catch (error) {
+    callback(error);
+  }
 };
